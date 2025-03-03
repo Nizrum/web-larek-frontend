a8a1054aaf02fd1c4eb1032efe2b9c2391b1abab
nizrum <nizrum215@yandex.ru>
2025-03-03 19:42:57 +0300

    feat: add basic types

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export interface IProductItem {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
+}
+
+export interface IActions {
+	onClick: (event: MouseEvent) => void;
+}
+
+export interface IOrderForm {
+	payment: string;
+	address: string;
+	phone: string;
+	email: string;
+	total: number;
+}
+
+export interface IOrder extends IOrderForm {
+	items: string[];
+}
+
+export interface IOrderResult {
+	id: string;
+	total: number;
+}
+
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
