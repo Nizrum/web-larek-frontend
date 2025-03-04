41c42d7c22687375b38df05050fbf88c238710da
nizrum <nizrum215@yandex.ru>
2025-03-04 22:57:51 +0300

    feat: add CartItemView

---
+++ src/components/views/CartItemView.ts
@@ -0,0 +1,49 @@
+import { IActions, IProductItem } from "../../types";
+import { IEvents } from "../base/events";
+import { cloneTemplate } from "../../utils/utils";
+
+export interface ICartItemView {
+	cartItem: HTMLElement;
+	index: HTMLElement;
+	title: HTMLElement;
+	price: HTMLElement;
+	deleteButton: HTMLButtonElement;
+	render(data: IProductItem, item: number): HTMLElement;
+}
+
+export class CartItemView implements ICartItemView {
+	cartItem: HTMLElement;
+	index: HTMLElement;
+	title: HTMLElement;
+	price: HTMLElement;
+	deleteButton: HTMLButtonElement;
+
+	constructor(
+		template: HTMLTemplateElement,
+		protected events: IEvents,
+		actions?: IActions
+	) {
+		this.cartItem = cloneTemplate<HTMLElement>(template);
+		this.index = this.cartItem.querySelector(".basket__item-index");
+		this.title = this.cartItem.querySelector(".card__title");
+		this.price = this.cartItem.querySelector(".card__price");
+		this.deleteButton = this.cartItem.querySelector(
+			".basket__item-delete"
+		);
+
+		if (actions?.onClick) {
+			this.deleteButton.addEventListener("click", actions.onClick);
+		}
+	}
+
+	getPriceText(value: null | number): string {
+        return value === null ? "Бесценно" : String(value) + " синапсов"
+    }
+
+	render(data: IProductItem, item: number) {
+		this.index.textContent = String(item);
+		this.title.textContent = data.title;
+		this.price.textContent = this.getPriceText(data.price);
+		return this.cartItem;
+	}
+}
