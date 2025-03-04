aeafbffcb182a180dc4c4b0b13efc810ace15113
nizrum <nizrum215@yandex.ru>
2025-03-04 16:25:49 +0300

    feat: add ProductListModel

---
+++ src/components/models/ProductListModel.ts
@@ -0,0 +1,31 @@
+import { IProductItem } from "../../types";
+import { IEvents } from "../base/events";
+
+export interface IProductListModel {
+  productCards: IProductItem[];
+  selectedСard: IProductItem;
+  showCard(item: IProductItem): void;
+}
+
+export class ProductListModel implements IProductListModel {
+  protected _productCards: IProductItem[];
+  selectedСard: IProductItem;
+
+  constructor(protected events: IEvents) {
+    this._productCards = []
+  }
+
+  set productCards(data: IProductItem[]) {
+    this._productCards = data;
+    this.events.emit('productCards:receive');
+  }
+
+  get productCards() {
+    return this._productCards;
+  }
+
+  showCard(item: IProductItem) {
+    this.selectedСard = item;
+    this.events.emit('modalCard:open', item)
+  }
+}
