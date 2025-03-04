ce887fbebe8549db0f6e8910d7f39eccee3a3aac
nizrum <nizrum215@yandex.ru>
2025-03-04 22:39:17 +0300

    feat: add CardModalView

---
+++ src/components/views/CardModalView.ts
@@ -0,0 +1,36 @@
+import { CardView } from "./CardView";
+import { IActions, IProductItem } from "../../types";
+import { IEvents } from "../base/events";
+
+export interface ICardModalView {
+	description: HTMLElement;
+	button: HTMLElement;
+	render(data: IProductItem): HTMLElement;
+}
+
+export class CardModalView extends CardView implements ICardModalView {
+	description: HTMLElement;
+	button: HTMLElement;
+
+	constructor(
+		template: HTMLTemplateElement,
+		protected events: IEvents,
+		actions?: IActions
+	) {
+		super(template, events, actions);
+		this.description = this._cardElement.querySelector(".card__text");
+		this.button = this._cardElement.querySelector(".card__button");
+		this.button.addEventListener("click", () => {
+			this.events.emit("card:addBasket");
+		});
+	}
+
+	render(data: IProductItem): HTMLElement {
+		super.render(data)
+		this.description.textContent = data.description;
+		if (!data.price) {
+            this.button.setAttribute("disabled", "true");
+        }
+		return this._cardElement;
+	}
+}
